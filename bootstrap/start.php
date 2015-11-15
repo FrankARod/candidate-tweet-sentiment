e70dc72268c48670e79bf354b50cdd56463d9ac2
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2015-11-15 13:20:03 -0600

    Removed hard-coded environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,6 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function () {
 
-	return 'local';
     return isset($_SERVER['LARAVEL_ENV']) && $_SERVER['LARAVEL_ENV'] == 'local' ? 'local' : 'production';
 
 });
